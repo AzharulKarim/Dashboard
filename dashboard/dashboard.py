d90d01a5422cdc784dda3a046c7f0dc93e0ec2a6
AzharulKarim <101646995+AzharulKarim@users.noreply.github.com>
2024-03-09 10:40:38 +0700

    Commit 4

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,10 +6,21 @@ import streamlit as st
 
 hours_dataset = pd.read_csv("dashboard/hour.csv")
 days_dataset = pd.read_csv("dashboard/day.csv")
-
+data_dashboard = pd.read_csv("dashboard/all_data.csv")
 
 season_cnt = days_dataset.groupby('season')['cnt'].sum().reset_index()
 weather_cnt = hours_dataset.groupby('weathersit')['cnt'].sum().reset_index()
+
+def create_daily_orders_df(df):
+    # Resample data harian dan hitung jumlah pesanan serta total pendapatan harian
+    daily_orders_df = df.resample(rule='D', on='dteday').agg({
+        "cnt": "sum",
+        "casual": "sum",
+        "registered": "sum"
+    })
+    daily_orders_df.reset_index(inplace=True)
+    return daily_orders_df
+
 
 def start():
     st.title('Proyek Analisis Data dengan Bike Sharing Dataset')
@@ -46,6 +57,27 @@ def start():
     """
     )
 
+
+datetime_columns = ["dteday"]
+data_dashboard.sort_values(by="dteday", inplace=True)
+data_dashboard.reset_index(inplace=True)
+ 
+for column in datetime_columns:
+    data_dashboard[column] = pd.to_datetime(data_dashboard[column])
+
+min_date = data_dashboard["dteday"].min()
+max_date = data_dashboard["dteday"].max()
+ 
+with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+    
+    # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
 if __name__ == "__main__":
     start()
 
